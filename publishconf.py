c3510a8f0e8b23b25654b883d3fc4f3bd303807e
Eban Rami <contact+git@eban.eu.org>
2020-11-28 15:52:58 +0000

    Update 'publishconf.py'

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://10.0.0.11:8000/'
+SITEURL = 'https://blog.eban.dev/'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
