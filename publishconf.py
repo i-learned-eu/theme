15d67821355ac83a94d7684cc13b2e1a99c45cd8
Eban <account@eban.dev>
2020-11-28 16:47:47 +0000

    Update 'publishconf.py'

--- publishconf.py
+++ publishconf.py
@@ -14,6 +14,7 @@ SITEURL = 'https://blog.eban.dev/'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
+FEED_RSS = 'rss.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
