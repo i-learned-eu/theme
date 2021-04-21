6ca8317d6e99cb40c0bea6fb941a0b7f483fef5d
Eban Rami <contact+git@eban.eu.org>
2021-04-21 11:08:33 +0200

    Added SEO plugin

--- pelicanconf.py
+++ pelicanconf.py
@@ -32,8 +32,14 @@ OUTPUT_SOURCES = False
 SUMMARY_MAX_LENGTH = 50
 THEME = 'theme/light'
 PLUGIN_PATHS = ['./pelican-plugins']
-PLUGINS = ['sitemap', 'post_stats', 'feed_summary', 'neighbors']
+PLUGINS = ['sitemap', 'post_stats', 'feed_summary', 'neighbors', 'seo']
 STATIC_PATHS = ['static/']
+
+# SEO settings
+SEO_REPORT = False  # To enable this feature
+SEO_ENHANCER = True  # To disable this feature
+SEO_ENHANCER_OPEN_GRAPH = True # The default value for this feature
+SEO_ENHANCER_TWITTER_CARDS = True #The default value for this feature
 
 SITEMAP = {
     "format": "xml",
