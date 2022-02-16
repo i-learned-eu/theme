7b458e411ac3c3782a24877f4a86bbfe711fd030
Eban Rami <account+gitil@eban.eu.org>
2022-02-16 16:49:07 +0000

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,6 +2,7 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: 'output/',
+      url: ["http://localhost/index.html", "http://localhost/about.html", "http://localhost/authors.html", "http://localhost/search.thml", "http://localhost/ipfs.html"],
     },
     upload: {
       target: 'temporary-public-storage',
