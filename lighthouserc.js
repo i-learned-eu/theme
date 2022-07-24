cdd8963beb67ae3a39c97f9b6216905e71674008
elmar <ramle+git@ramle.be>
2022-07-24 23:45:14 +0200

    update commit+use artefacts for output

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,10 +2,10 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: 'output/',
-      url: ["http://localhost/index.html", "http://localhost/about.html", "http://localhost/authors.html", "http://localhost/search.html", "http://localhost/ipfs.html"],
+      url: ["http://localhost/index.html", "http://localhost/latest.html", "http://localhost/search.html", "http://localhost/ipfs.html"],
     },
     upload: {
-      target: 'temporary-public-storage',
+      target: 'filesystem',
     },
   },
 };
